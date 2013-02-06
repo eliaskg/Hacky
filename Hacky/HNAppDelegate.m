0e6dd1237d8a8f9a6007bee1da33d6c8fa0eafd8
Elias Klughammer <elias.klughammer@me.com>
2013-02-06 11:56:01 +0100

    Started with scraping

--- Hacky/HNAppDelegate.m
+++ Hacky/HNAppDelegate.m
@@ -235,15 +235,112 @@
 
 - (void)parse:(NSString*)response
 {
+  NSMutableArray *stories = [[NSMutableArray alloc] init];
   
-//  NSLog(@"now:");
-//  NSData* data = [response dataUsingEncoding:NSUTF8StringEncoding];
-//  TFHpple* doc = [[TFHpple alloc] initWithHTMLData:data];
+  NSData* data = [response dataUsingEncoding:NSUTF8StringEncoding];
+  TFHpple* doc = [[TFHpple alloc] initWithHTMLData:data];
+  
+  NSArray* tables = [doc searchWithXPathQuery:@"//table[not(@width)]"];
+  
+  TFHppleElement* mainTable = tables[0];
+  
+  NSArray* trs_ = [mainTable childrenWithTagName:@"tr"];
+  NSMutableArray* trs = [NSMutableArray arrayWithArray:trs_];
+  
+  // --- Remove the "more" button
+  [trs removeLastObject];
+  [trs removeLastObject];
+  
+  int j = 0;
+  
+  NSMutableDictionary* story = [NSMutableDictionary dictionaryWithCapacity:7];
+  
+  for (int i = 0; i < [trs count]; i++) {
+    TFHppleElement* tr = trs[i];
+    
+    // --- First row of story (title)
+    if (i % 3 == 0) {
+      // --- Get title and URL
+      NSArray *titleTds = [tr childrenWithClassName:@"title"];
+      TFHppleElement *titleTd = [titleTds objectAtIndex:1];
+      TFHppleElement *titleA = [titleTd firstChildWithTagName:@"a"];
+      [story setValue:[titleA text] forKey:@"title"];
+      [story setValue:[titleA objectForKey:@"href"] forKey:@"url"];
+      
+      // --- Get id
+      NSArray* tds = [tr childrenWithTagName:@"td"];
+      TFHppleElement* upvoteTd = [tds objectAtIndex:1];
+      TFHppleElement* upvoteCenter = [upvoteTd firstChildWithTagName:@"center"];
+      TFHppleElement* upvoteA = [upvoteCenter firstChildWithTagName:@"a"];
+      NSString* upvoteId = [upvoteA objectForKey:@"id"];
+      NSString* storyId = [[upvoteId componentsSeparatedByString:@"_"] objectAtIndex:1];
+      [story setValue:storyId forKey:@"id"];
+    }
+    // --- Second row of story (comment count, timestamp, etc)
+    else if (i % 3 == 1) {
+      NSArray* metaTds = [tr childrenWithClassName:@"subtext"];
+      TFHppleElement* metaTd = [metaTds objectAtIndex:0];
+      
+      NSArray* metaAs = [metaTd childrenWithTagName:@"a"];
+      
+      // --- Get score
+      TFHppleElement* scoreSpan = [metaTd firstChildWithTagName:@"span"];
+      NSString* scoreText = [scoreSpan text];
+      NSString* score = [NSString stringWithFormat:@"%li", [self numberFromString:scoreText]];
+      [story setValue:score forKey:@"score"];
+      
+      // --- Get username
+      TFHppleElement* userA = [metaAs objectAtIndex:0];
+      [story setValue:[userA text] forKey:@"user"];
+      
+      // --- Get comments count
+      TFHppleElement* commentsA = [metaAs lastObject];
+      NSString* commentsText = [commentsA text];
+      NSString* comments = [NSString stringWithFormat:@"%li", [self numberFromString:commentsText]];
+      [story setValue:comments forKey:@"comments"];
+      
+      NSLog([metaTd text]);
+      
+      // -- Add a copy of the story to the array
+      NSMutableDictionary* story_ = [story copy];
+      [stories addObject:story_];
+//      j++;
+    }
+  }
+  
+//  NSLog(@"%@", stories);
+  
 //  NSArray* titles = [doc searchWithXPathQuery:@"//*[contains(@class,'title')] "];
 //  
 //  for (int i = 0; i < [titles count]; i++) {
-//    NSLog(@"%@", [titles[i] text]);
-//  }
+//    TFHppleElement* element = titles[i];
+////    NSLog(@"%d", i % 2);
+//    if (i % 2 == 1) {
+////      NSLog([element tagName]);
+//    }
+}
+
+- (NSInteger)numberFromString:(NSString*)string
+{
+  // Input
+  NSString *originalString = string;
+  
+  // Intermediate
+  NSString *numberString;
+  
+  NSScanner *scanner = [NSScanner scannerWithString:originalString];
+  NSCharacterSet *numbers = [NSCharacterSet characterSetWithCharactersInString:@"0123456789"];
+  
+  // Throw away characters before the first number.
+  [scanner scanUpToCharactersFromSet:numbers intoString:NULL];
+  
+  // Collect numbers.
+  [scanner scanCharactersFromSet:numbers intoString:&numberString];
+  
+  // Result.
+  int number = [numberString integerValue];
+  
+  return number;
 }
 
 @end
