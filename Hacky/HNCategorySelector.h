ddcd2f731ac61fbae0104dc1b2e822e7976dc4c9
Elias Klughammer <elias.klughammer@me.com>
2013-03-02 20:50:37 +0100

    Started with category selector

---
+++ Hacky/HNCategorySelector.h
@@ -0,0 +1,13 @@
+//
+//  HNCategorySelector.h
+//  Hacky
+//
+//  Created by Elias Klughammer on 02.03.13.
+//  Copyright (c) 2013 Elias Klughammer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HNCategorySelector : NSControl
+
+@end
---
+++ Hacky/HNCategorySelector.m
@@ -0,0 +1,28 @@
+//
+//  HNCategorySelector.m
+//  Hacky
+//
+//  Created by Elias Klughammer on 02.03.13.
+//  Copyright (c) 2013 Elias Klughammer. All rights reserved.
+//
+
+#import "HNCategorySelector.h"
+
+@implementation HNCategorySelector
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
