643dd8c82ad1b1e8a333841b800b96291a701835
Elias Klughammer <elias.klughammer@me.com>
2013-03-02 22:06:37 +0100

    Implemented Category Button

--- Hacky/HNCategorySelector.h
+++ Hacky/HNCategorySelector.h
@@ -9,5 +9,22 @@
 #import <Cocoa/Cocoa.h>
 
 @interface HNCategorySelector : NSControl
+{
+  NSString* title;
+  NSTextField* titleLabel;
+  NSImage* dropDownImageNormal;
+  NSImage* dropDownImageActive;
+  NSImageView* dropDownImageView;
+  NSTrackingArea* trackingArea;
+  NSMenu* menu;
+}
+
+@property (nonatomic, retain) NSString* title;
+@property (nonatomic, retain) NSTextField* titleLabel;
+@property (nonatomic, retain) NSImage* dropDownImageNormal;
+@property (nonatomic, retain) NSImage* dropDownImageActive;
+@property (nonatomic, retain) NSImageView* dropDownImageView;
+@property (nonatomic, retain) NSTrackingArea* trackingArea;
+@property (nonatomic, retain) NSMenu* menu;
 
 @end
